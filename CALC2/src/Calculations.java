fae75567f742f73466d5c56fb33c5b05c7cc56c2
Grace-Joy <33892212+Grace-Joy@users.noreply.github.com>
2018-01-16 10:39:46 +0300

    Add files via upload

---
+++ CALC2/src/Calculations.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+public class Calculations {
+	public static void main(String[] args) {
+		Calcu c= new Calcu();
+		c.setSize(450, 400);
+		c.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		c.setVisible(true);
+		c.setResizable(false);
+	}
+}
